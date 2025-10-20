d7c17f6d2927c6fdba2e19f7d673f385918aedaf
sophia144 <piercesp@outlook.com>
2025-10-20 12:59:52 +0100

    Began data cleaning

--- analysis.py
+++ analysis.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from pandasql import sqldf
 
+df = (pd.read_csv('sony_stocks.csv'))
+df = df.rename(columns={"Close/Last": "Closing"})
+df = df.astype({'Date': 'datetime64[ns]'})
+
+df["Closing"] = df["Closing"].str.replace('$', '')
+
+print(df)
