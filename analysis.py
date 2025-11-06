4483d0333904dca107be14b92c27b0b8b23458ff
sophia144 <piercesp@outlook.com>
2025-11-06 09:59:01 +0000

    Adding comments

--- analysis.py
+++ analysis.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pandasql import sqldf
 
+#importing and cleaning
 df = (pd.read_csv('sony_stocks.csv'))
 df = df.rename(columns={"Close/Last": "Closing"})
 df = df.astype({'Date': 'datetime64[ns]'})
-
 df["Closing"] = df["Closing"].str.replace('$', '')
 
+#for debugging - delete later
 print(df)
