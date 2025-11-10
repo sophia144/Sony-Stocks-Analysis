a898d69e84dc78ccc2796c0b36b4894b3527fe1f
sophia144 <piercesp@outlook.com>
2025-11-10 11:37:51 +0000

    Graphed the time taken to sort a dataframe against the number of rows

--- sorting_experimentation.py
+++ sorting_experimentation.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from time import perf_counter
 
 df = (pd.read_csv('sony_stocks_cleaned.csv'))
-df["Daily Change"] = df["Open"].diff()
+df["Daily Change"] = df["Closing"].diff()
 
 def sort_time_calc(n):
     df_slice = df[1:n+1]
     start_time = perf_counter()
-    sorted_slice = df_slice.sort_values(by="Daily Change", ignore_index=True)
+    sorted_slice = df_slice.sort_values(by="Daily Change")
     end_time = perf_counter()
-    sort_time = start_time - end_time
+    sort_time = end_time - start_time
     sort_time_list.append([n, sort_time])
 
 sort_time_list = []
@@ -20,3 +20,11 @@ for n in range(7, 365):
 
 x = [value[0] for value in sort_time_list]
 y = [value[1] for value in sort_time_list]
+
+plt.plot(x, y)
+plt.show()
+
+plt.xlabel("Number of Rows")
+plt.ylabel("Sort Time (seconds)")
+plt.title("Sort Time vs Data Size")
+plt.grid(True)
