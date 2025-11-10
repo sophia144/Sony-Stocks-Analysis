8228cd4c4cac55fd0a443584b5e816dfa09a2584
sophia144 <piercesp@outlook.com>
2025-11-10 11:30:41 +0000

    Added a function to time how long sorting different slices of the dataframe takes

--- analysis.py
+++ analysis.py
@@ -1,11 +1,16 @@
 import pandas as pd
-from pandasql import sqldf
 
 #importing and cleaning
 df = (pd.read_csv('sony_stocks.csv'))
 df = df.rename(columns={"Close/Last": "Closing"})
 df = df.astype({'Date': 'datetime64[ns]'})
-df["Closing"] = df["Closing"].str.replace('$', '')
 
-#for debugging - delete later
+df["Closing"] = df["Closing"].str.replace('$', '')
+df["Open"] = df["Open"].str.replace('$', '')
+df["High"] = df["High"].str.replace('$', '')
+df["Low"] = df["Low"].str.replace('$', '')
+
+df.sort_values(by="Date", inplace=True, ignore_index=True)
+df.to_csv('sony_stocks_cleaned.csv', index=False)
+
 print(df)
--- sorting_experimentation.py
+++ sorting_experimentation.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from time import perf_counter
 
+df = (pd.read_csv('sony_stocks_cleaned.csv'))
+df["Daily Change"] = df["Open"].diff()
+
+def sort_time_calc(n):
+    df_slice = df[1:n+1]
+    start_time = perf_counter()
+    sorted_slice = df_slice.sort_values(by="Daily Change", ignore_index=True)
+    end_time = perf_counter()
+    sort_time = start_time - end_time
+    sort_time_list.append([n, sort_time])
+
+sort_time_list = []
+
+for n in range(7, 365):
+    sort_time_calc(n)
+
+x = [value[0] for value in sort_time_list]
+y = [value[1] for value in sort_time_list]
